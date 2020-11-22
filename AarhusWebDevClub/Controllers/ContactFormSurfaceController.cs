6235a27eb1a32786cf6fcbcf4dbf9ab76f3357c2
nbjessen <nbj@jessens.com>
2020-11-22 14:03:38 +0100

    Add project files.

---
+++ AarhusWebDevClub/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AarhusWebDevClub.ViewModels;
+using Umbraco.Web.Mvc;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevClub.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+          
+
+            if (!ModelState.IsValid)
+            return CurrentUmbracoPage();
+            TempData["success"] = true;
+
+            IContent comment = Services.ContentService.Create(model.Subject, CurrentPage.Id, "comment");
+            comment.SetValue("username", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("message", model.Message);
+            Services.ContentService.Save(comment);
+
+            return RedirectToCurrentUmbracoPage();
+
+
+
+        }
+    }  
+}
---
+++ readme.txt
@@ -0,0 +1,26 @@
+Login:
+nbj@jessens.com
+
+Password:
+1234567890
+
+Active User:
+Nicolai
+1234567890
+
+Prospect:
+Helene
+1234567890
+
+Package:
+SmallerImages
+
+Description:
+Reducer størrelsen på billeder så de ikke fylder deres faktiske størrelse på siden.
+Det er sat således op at et billede kan maks have en højde på 600px og bredde på 1067px.
+Aspektratio bliver ikke ændret
+
+Introducing Umbraco Package - SmallerImages
+https://www.youtube.com/watch?v=gcHhGmei8AM
+
+https://github.com/prjseal/SmallerImages/blob/master/README.md
